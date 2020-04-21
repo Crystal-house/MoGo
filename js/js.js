110ededa3d13474e16151cf42c1702e828aa0368
Andrey <loc.allsupport@yandex.ru>
2020-04-21 17:58:48 +0300

    added slider

--- js/js.js
+++ js/js.js
@@ -1,2 +1,34 @@
+/*Owl carousel 2*/
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    dots: false,
+    nav:false,
+    smartSpeed:700,
 
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
+        }
+    }
+})
+
+//Делегируем события кнопок next prev по умолчанию нашим кнопкам, которые могут находится вне контейнера слайдера
+var owl=$(".owl-carousel");
+owl.owlCarousel();
+
+$(".slider__owl-next").click(function(){
+    owl.trigger("next.owl.carousel");
+});
+$(".slider__owl-prev").click(function(){
+    owl.trigger("prev.owl.carousel");
+});
+
+/*/Owl carousel 2*/
 
