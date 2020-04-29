f54cdaff9d0d9ab10d4102e02accc166af696c15
Andrey <loc.allsupport@yandex.ru>
2020-04-29 21:23:43 +0300

    add partners block

--- js/js.js
+++ js/js.js
@@ -1,34 +1,74 @@
-/*Owl carousel 2*/
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:3,
-    dots: false,
-    nav:false,
-    smartSpeed:700,
+function owlCarousel1(){
+	$('.slider_owl-carousel-1').owlCarousel({
+	    loop:true,
+	    margin:3,
+	    dots: false,
+	    nav:false,
+	    smartSpeed:700,
+	    responsive:{
+	        0:{
+	            items:1,
+	        },
+	        600:{
+	            items:1,
+	        },
+	        1000:{
+	            items:1,
+	        }
+	    }
+	})
+	//Делегируем события кнопок next prev по умолчанию нашим кнопкам, которые могут находится вне контейнера слайдера
+	var owl=$(".slider_owl-carousel-1");
+	owl.owlCarousel();
 
-    responsive:{
-        0:{
-            items:1,
-        },
-        600:{
-            items:1,
-        },
-        1000:{
-            items:1,
-        }
-    }
-})
+	$(".slider__owl-next").click(function(){
+	    owl.trigger("next.owl.carousel");
+	});
+	$(".slider__owl-prev").click(function(){
+	    owl.trigger("prev.owl.carousel");
+	});	
+}
 
-//Делегируем события кнопок next prev по умолчанию нашим кнопкам, которые могут находится вне контейнера слайдера
-var owl=$(".owl-carousel");
-owl.owlCarousel();
+function owlCarousel2(){
+$('.slider_owl-carousel-2').owlCarousel({
+	    responsive:{
+	    	0:{
+	            items:3,
+	            margin:15,
+	        },
+	        350:{
+	            items:4,
+	            margin:15,
+	        },
+	        600:{
+	            items:6,
+	            margin:15,
+	        },
 
-$(".slider__owl-next").click(function(){
-    owl.trigger("next.owl.carousel");
+	        1000:{
+	            items:6,
+	        }
+	    }
+	})
+}	
+	
+	
+
+
+
+
+$(document).ready(function(){
+	owlCarousel1();
+	owlCarousel2();
 });
-$(".slider__owl-prev").click(function(){
-    owl.trigger("prev.owl.carousel");
-});
 
-/*/Owl carousel 2*/
+
+
+
+
+
+
+
+
+
 
